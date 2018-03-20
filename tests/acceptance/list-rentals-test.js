3ecf647027295effef1d94ec8d7bc7fe7eed8664
millarba <millarba@gmail.com>
2018-03-20 17:53:09 -0500

    add menu class to navigation buttons and fix tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -15,23 +15,27 @@ module('Acceptance | my acceptance test', function(hooks) {
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
-  test('should link to information about the company', function(assert) {
+  test('should link to about page', async function(assert) {
+    await visit('/');
+    await click('.menu-about');
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information', async function(assert) {
+    await visit('/');
+    await click('.menu-contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+
+  test('should list available rentals', async function(assert) {
 
   });
 
-  test('should link to contact information', function(assert) {
+  test('should filter the list of rentals by city', async function(assert) {
 
   });
 
-  test('should list available rentals', function(assert) {
-
-  });
-
-  test('should filter the list of rentals by city', function(assert) {
-
-  });
-
-  test('should show details for a selected rental', function(assert) {
+  test('should show details for a selected rental', async function(assert) {
 
   });
 });
