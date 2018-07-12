37dad5dc64799ef400c1242824460c4694c3f564
millarba <millarba@gmail.com>
2018-07-11 21:44:26 -0500

    add test for all listings to list-filter

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -3,24 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test("should initially load all listings", function(assert) {
 
-    await render(hbs`{{list-filter}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#list-filter}}
-        template block text
-      {{/list-filter}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
