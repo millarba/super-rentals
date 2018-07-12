122f32f6577b10626e43c621b2431095af620188
millarba <millarba@gmail.com>
2018-07-11 21:51:51 -0500

    last commit mid test

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -9,7 +9,24 @@ const FILTERED_ITEMS = [{city: 'San Francsico'}];
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test("should initially load all listings", function(assert) {
+  test("should initially load all listings", async function(assert) {
+    // we want our actions to return promises,
+    //since they are potentially fetching data asynchronously
+    this.set('filterByCity', () => resolve({ results: ITEMS }));
 
+    // with an integration test,
+    // you can set up and use your component in the same way your application
+    // will use it.
+    await render(hbs`
+      {{#list-filter filter=(action filterByCity) as |results|}}
+        <ul>
+        {{#each results as |item|}}
+          <li class="city">
+            {{item.city}}
+          </li>
+        {{/each}}
+        </ul>
+      {{/list-filter}}
+    `);
   });
 });
