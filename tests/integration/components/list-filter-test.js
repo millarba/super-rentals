f83eb063573e1d042c7701cff77518fe089fe8c7
millarba <millarba@gmail.com>
2018-07-11 21:47:50 -0500

    add test data for list-filter

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -3,6 +3,8 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francsico'}];
 
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
